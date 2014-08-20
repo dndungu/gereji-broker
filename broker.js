d37ab1384bfbaa0a46db552099a46293d4ed349d
David Njuguna <dnjuguna@gmail.com>
2014-08-20 21:57:04 +0300

    initial

--- broker.js
+++ broker.js
@@ -21,31 +21,21 @@ module.exports = function(){
 				self.events[type].push(arguments[1]);
 			}
 		},
-		emit : function(_event) {
-			_event = typeof _event == "string" ? {type : _event, data : {}} : _event;
-			_event.data = typeof _event.data == "undefined" ? {} : _event.data;
+		emit : function(event) {
+			event = typeof event == "string" ? {type : event, data : {}} : event;
+			event.data = typeof event.data == "undefined" ? {} : event.data;
 			try{
-				this.log(5, _event);
-				var listeners = self.events[_event.type];
+				self.store.context.log(5, event);
+				var listeners = self.events[event.type];
 				if(!listeners)
-					throw new Error('There  are no event listeners for ' + _event.type);
+					throw new Error('There  are no event listeners for ' + event.type);
 				for(var i in listeners){
-					typeof listeners[i] === 'function' && listeners[i](_event);
+					if(typeof listeners[i] === 'function')
+						listeners[i](event);
 				}
 			}catch(error){
-				this.log(2, error.stack);
+				self.store.context.log(2, error.stack);
 			}
-		},
-        log: function(severity, message){
-			var settings = self.store.context.get("settings");
-            var logfile = settings.debug.file;
-            var uri = self.store.context.get("uri");
-            var method = self.store.context.get("method");
-            if(severity > settings.debug.level)
-				return;
-            var line = (new Date()) + ' (' + severity + ') : [' + uri + '] ' + ' {' + method + '} ' + (JSON.stringify(message));
-            fs.appendFile(logfile, line, function(){});
-            return this;
-        }
+		}
 	}
 };
